9295c3ebd24fa96da6f00d9545ab5a67e87fe3d3
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2023-08-29 09:07:48 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import TeamMembersTable from './components/TeamMembersTable';
+
+const App = () => {
+    return (
+        <div>
+            {/* Other components */}
+            <TeamMembersTable />
+        </div>
+    );
+};
+
+export default App;
